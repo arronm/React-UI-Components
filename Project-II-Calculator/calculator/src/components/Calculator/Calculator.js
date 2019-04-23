a1a9e371d37d394fa8d71487cf9d2ed29aaad167
Arron M <marshall.arron@gmail.com>
2019-04-23 13:40:54 -0700

    Add order of operations calculation functionality

--- Project-II-Calculator/calculator/src/components/Calculator/Calculator.js
+++ Project-II-Calculator/calculator/src/components/Calculator/Calculator.js
@@ -8,31 +8,64 @@ class Calculator extends Component {
     super(props);
     this.state = { 
       calculation: '0',
-     }
+      calculated: false,
+    }
     this.updateCalculation = this.updateCalculation.bind(this);
-  }
 
-  calculate() {
-    // calculate from string
-    return 42;
-  }
-
-  updateCalculation = (string) => {
-    const operators = {
-      "+": true,
-      "*": true,
-      "/": true,
-      "-": true,
+    // TODO: Probably a better way to manage these functions, perhaps eval?
+    this.operators = {
+      "+": (operation) => {
+        const numbers = operation[0].split("+");
+        const result = parseInt(numbers[0]) + parseInt(numbers[1]);
+        const calculation = operation.input.replace(operation[0], result);
+        return calculation;
+      },
+      "*": (operation) => {
+        const numbers = operation[0].split("*");
+        const result = parseInt(numbers[0]) * parseInt(numbers[1]);
+        const calculation = operation.input.replace(operation[0], result);
+        return calculation;
+      },
+      "/": (operation) => {
+        const numbers = operation[0].split("/");
+        const result = parseInt(numbers[0]) / parseInt(numbers[1]);
+        const calculation = operation.input.replace(operation[0], result);
+        return calculation;
+      },
+      "-": (operation) => {
+        const numbers = operation[0].split("-");
+        const result = parseInt(numbers[0]) - parseInt(numbers[1]);
+        const calculation = operation.input.replace(operation[0], result);
+        return calculation;
+      },
       "=": true,
       "c": true,
     };
+  }
 
-    if (operators[string]) {
+  calculate(calculation) {
+    if (!/\D/.exec(calculation)) {
+      return calculation;
+    }
+
+    let operation = /\d*(\*|\/)\d*/.exec(calculation);
+    if (operation === null) {
+      operation = /\d*(\+|\-)\d*/.exec(calculation);
+      if (!operation) return calculation;
+    };
+
+    let operator = /\D/.exec(operation);
+    return this.calculate(this.operators[operator](operation));
+  }
+
+  updateCalculation = (string) => {
+    if (this.operators[string]) {
       if (string === '=') {
-        const result = this.calculate();
+        const result = this.calculate(this.state.calculation);
         this.setState({
           ...this.state,
           calculation: result,
+          calculated: true,
         });
         return;
       }
@@ -41,20 +74,41 @@ class Calculator extends Component {
         this.setState({
           ...this.state,
           calculation: '0',
+          calculated: false,
         });
       } else {
+        if (this.operators[this.state.calculation[this.state.calculation.length - 1]]) {
+          console.log(this.state.calculation);
+          string = this.state.calculation.replace(/.$/, string);
+          this.setState({
+            ...this.state,
+            calculation: string,
+            calculated: false,
+          });
+          return;
+        }
+
         this.setState({
           ...this.state,
           calculation: this.state.calculation === '0' ? '0' : this.state.calculation + string,
+          calculated: false,
         });
       }
       return;
     }
     
-    this.setState({
-      ...this.state,
-      calculation: this.state.calculation === '0' ? string : this.state.calculation + string,
-    });
+    if (this.state.calculated) {
+      this.setState({
+        ...this.state,
+        calculation: string,
+        calculated: false,
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        calculation: this.state.calculation === '0' ? string : this.state.calculation + string,
+      });
+    }
   }
 
   render() {
